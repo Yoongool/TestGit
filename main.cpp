0c3c158c03dbeb5df0c987a3907b3d7a2f5a7ab4
yong.yao <yong.yao@thundersoft.com>
2022-08-18 11:17:06 +0800

    444

--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 
-int add(int a, int b)
-{
+int add(int a, int b){
     return a+b;
 }
 
-float add2(float a, float b)
-{
+float add2(float a, float b){
     return a+b;
 }
 
-int main()
-{
+int main(){
     std::cout << "Hello world!" << std::endl;
     std::cout << "a+b = " << add(1,2) << std::endl;
     return 0;
